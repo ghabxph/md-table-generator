5586280e17d5b3a4b5cc595b141cb9679178024e
ghabxph <11975597+ghabxph@users.noreply.github.com>
2019-12-27 13:24:32 +0800

    Removed unnecessary code

--- script.py
+++ script.py
@@ -3,8 +3,4 @@
 import sys
 from src.tablecreator import TableCreator
 
-row = int(sys.argv[1])
-col = int(sys.argv[2])
-size = int(sys.argv[3])
-
 print(TableCreator(sys.argv[1], sys.argv[2], sys.argv[3]).create_table().to_string())
